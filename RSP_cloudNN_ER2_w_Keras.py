bc650715c2efcb2644e8fcc5c3f2c6d17ff3c6c8
segalm75 <segalm75@users.noreply.github.com>
2017-12-27 15:50:57 -0800

    creating optimization routine

--- RSP_cloudNN_ER2_w_Keras.py
+++ RSP_cloudNN_ER2_w_Keras.py
@@ -15,7 +15,7 @@
 # 9. compile and fit
 # 10.test model (evaluate)
 
-# In[1]:
+# In[25]:
 
 
 # import neccesary modules for this module
@@ -40,6 +40,8 @@ import shapefile
 # keras related libraries
 from keras.models import Sequential # import model
 from keras.layers import Dense, Dropout, Activation, Flatten # import core layers
+from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping, ModelCheckpoint
+from keras.optimizers import Adam
 from keras.utils import np_utils # import helper funcs
 from keras.regularizers import l2 # import l2 regularizer
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -279,6 +281,15 @@ file_name = 'D://ORACLES_NN//py_inputs//ER2_x_dolp.csv'
 dolp_x.to_csv(file_name, header=True, index=False)
 
 
+# In[40]:
+
+
+# all inputs together:
+ref_i_ref_q_dolp = pd.concat([azi_std, sza_std, ref_i_std, ref_q_std, dolp_std], axis=1)
+file_name = 'D://ORACLES_NN//py_inputs//ER2_x_ref_i_ref_q_dolp.csv'
+ref_i_ref_q_dolp.to_csv(file_name, header=True, index=False)
+
+
 # In[15]:
 
 
@@ -314,9 +325,269 @@ for f in range(len(var_in)):
     print("y_train_sample",y_train[0:5])
 
 
-# In[17]:
+# In[20]:
 
 
 # 8. build model
 #-----------------
+model = Sequential()
 
+model.add(Dropout(0.50, input_shape=X_train.shape[1:], name="drop_input"))
+model.add(Dense(1024, activation='relu', name="fc1"))
+model.add(Dropout(0.50, name="drop_fc1"))
+model.add(Dense(1024, activation='relu', name="fc2"))
+model.add(Dropout(0.50, name="drop_fc2"))
+model.add(Dense(1024, activation='relu', name="fc3"))
+model.add(Dropout(0.50, name="drop_fc3"))
+model.add(Dense(y_test.shape[1], activation='linear',name='out_linear'))
+
+model.summary()
+
+
+# In[36]:
+
+
+# 8. + 9. compile and fit
+#------------------------
+
+# optimizer
+adam = Adam(lr=0.0001)
+model.compile(loss='mean_squared_error', 
+              optimizer=adam,
+              metrics=['accuracy'])
+# early stopping conditions:
+earlystopping = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=0, mode='auto')
+# saving best model params
+checkpointer = ModelCheckpoint(filepath="D://ORACLES_NN//py_outputs//ER2_model_weights.hdf5", verbose=0, 
+                               mode='max', save_best_only=True)
+csv_logger  = CSVLogger('D://ORACLES_NN//py_outputs//ER2_model_log.csv', 
+                                   append=True, separator=';')
+    
+#hist = model.fit(..., callbacks=[checkpointer])
+#model.load_weights('weights.hdf5')
+#predicted = model.predict(X_test_mat)
+
+# using callbacks to stop/save
+#callbacks = [
+#    EarlyStopping(monitor='val_loss', patience=2, verbose=0),
+#    ModelCheckpoint(kfold_weights_path, monitor='val_loss', save_best_only=True, verbose=0),
+#]
+
+#callbacks = [EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=0, mode='auto')]
+#callbacks = [EarlyStopping,checkpointer,csv_logger]
+
+X_train = X_train.astype('float32')
+X_test  = X_test.astype('float32')
+#print(X_train.head())
+model.fit(x = np.array(X_train), y=np.array(y_train),
+          epochs=2, batch_size=32,verbose=1,validation_split=0.15, 
+          callbacks = [earlystopping,checkpointer,csv_logger])
+
+
+# In[37]:
+
+
+# 10. model evaluate
+#-------------------
+score, acc = model.evaluate(np.array(X_test), np.array(y_test), batch_size=32)
+print("score is: ", score, "accuracy is: ", acc)
+
+
+# ## 11. this step is to run all model options
+
+# In[42]:
+
+
+# this function splits train/test sets from data for each of the input variables
+#--------------------------------------------------------------------------------
+# pre_file is a str describing the path and pre_file name for input csv file to split,
+# e.g. 'D://ORACLES_NN//py_inputs//ER2_x_
+# var_in is the variable input combination str e.g. "ref_i_dolp"
+# var_out is file path and name for the output csv file, 
+# e.g.: 'D://ORACLES_NN//py_inputs//ER2_y_train_orig.csv'
+#
+def split_data(pre_file, var_in, var_out):
+    
+    from sklearn import preprocessing
+    from sklearn.model_selection import train_test_split
+    import numpy as np
+    
+    # import x
+    x_filename = pre_file + var_in + '.csv'
+    X = pd.read_csv(x_filename)
+    #print(X.head())
+    #import y
+    y = pd.read_csv(var_out)
+    #print(y.head())
+    # import train/validation data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42,shuffle=True)
+
+    # print and plot data samples
+
+    print("X_shape",      X.shape)
+    print("X_train_shape",X_train.shape)
+    print("X_test_shape" ,X_test.shape)
+    print("X_train_shape[0]",X_train.shape[0])
+    print("y_train_shape",y_train.shape)
+    print("y_test_shape" ,y_test.shape)
+    print("y_train_sample",y_train[0:5])
+    
+    # convert to keras input type:
+    X_train = X_train.astype('float32')
+    X_test  = X_test.astype('float32')
+    y_train = y_train.astype('float32')
+    y_test  = y_test.astype('float32')
+    
+    
+    return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
+
+
+# In[43]:
+
+
+# this function defines the model to run
+#----------------------------------------
+# input shape is tensor shape (without number of samples), i.e.X_train.shape[1:]
+# output shape is tensor shape of the output (without number of samples), i.e. y_test.shape[1]
+#
+
+def model(input_shape, output_shape):
+
+    model = Sequential()
+
+    model.add(Dropout(0.50, input_shape=input_shape, name="drop_input"))
+    model.add(Dense(1024, activation='relu', name="fc1"))
+    model.add(Dropout(0.50, name="drop_fc1"))
+    model.add(Dense(1024, activation='relu', name="fc2"))
+    model.add(Dropout(0.50, name="drop_fc2"))
+    model.add(Dense(1024, activation='relu', name="fc3"))
+    model.add(Dropout(0.50, name="drop_fc3"))
+    model.add(Dense(output_shape, activation='linear',name='out_linear'))
+
+    model.summary()
+    
+    return model
+
+
+# In[56]:
+
+
+# this function runs the model (compile, fit, and evaluate on test)
+#---------------------------------------------------------------------
+# model - is model generated by model function
+# lr - learning rate
+# decay - optimizer decay rate (regularizer)
+# var_in - str with variable combination for saving model run results
+# file_str - str to save run files (e.g. ER2 or P3)
+# X_train, X_test, y_train, y_test are outputs from split_data()
+#
+
+def run_model(model, var_in, file_str, X_train, X_test, y_train, y_test,lr=0.0001, decay=1e-3):
+    
+    import numpy
+    
+    # optimizer
+    adam = Adam(lr=lr,decay=decay)
+    model.compile(loss='mean_squared_error', 
+                  optimizer=adam,
+                  metrics=['accuracy'])
+    
+    # early stopping conditions:
+    earlystopping = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=5, verbose=0, mode='auto')
+    # saving best model params
+    checkpointer = ModelCheckpoint(filepath="D://ORACLES_NN//py_outputs//" + file_str + var_in + "_model_weights.hdf5", 
+                                   verbose=0, 
+                                   mode='max', save_best_only=True)
+    csv_logger  = CSVLogger("D://ORACLES_NN//py_outputs//" + file_str + var_in + "_model_log.csv", 
+                                       append=True, separator=';')
+
+    callbacks = [earlystopping,checkpointer,csv_logger]
+
+    history = model.fit(x = X_train, y=y_train,
+              epochs=30, batch_size=64,verbose=1,validation_split=0.15, 
+              callbacks = callbacks)
+    
+    h1   = history.history
+    acc_ = numpy.asarray(h1['acc'])
+    loss_ = numpy.asarray(h1['loss'])
+    val_loss_ = numpy.asarray(h1['val_loss'])
+    val_acc_  = numpy.asarray(h1['val_acc'])
+    
+    opt        = "Adam"
+    lr         = lr
+    decay      = decay
+
+
+    acc_plot = "D://ORACLES_NN//py_plots//" + file_str + "_accuracy_run_" + var_in + ".png"
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('run: ' + var_in + " opt: " + opt + " lr: " + str(lr) + " decay: " + str(decay))
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(acc_plot)   
+    plt.close()  
+    
+    los_plot = "D://ORACLES_NN//py_plots//" + file_str + "_losses_run_" + var_in + ".png"
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('run: ' + var_in + " opt: " + opt + " lr: " + str(lr) + " decay: " + str(decay))
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(los_plot)   
+    plt.close()  
+    
+
+    acc_and_loss = numpy.column_stack((acc_, loss_, val_acc_, val_loss_))
+    save_file_mlp = "D://ORACLES_NN//py_outputs//" + file_str + 'mlp_run_'  + var_in + '.txt'
+    with open(save_file_mlp, 'w') as f:
+            numpy.savetxt(save_file_mlp, acc_and_loss, delimiter=",")
+
+    score, acc = model.evaluate(X_test, y_test, batch_size=64, verbose=0)
+    print("parameters for run " + var_in + ":")
+    print("-------------------------------")
+
+    print("opt: ", opt)
+    print("lr: ", lr)
+    print("decay: ", decay)
+    print("val_accuracy: ",val_acc_)
+    print("Test accuracy is: ", acc)
+
+    save_file_params = "D://ORACLES_NN//py_outputs//" + file_str + 'params_run_' + var_in + '.txt'
+    rownames  = numpy.array(['Run', 'optimizer', 'learning_rate', 'decay', 'train_accuracy','train_loss',
+                             'val_accuracy', 'val_loss', 'test_accuracy'])
+    rowvals   = (var_in, opt, lr, decay, acc_[-1], loss_[-1], val_acc_[-1], val_loss_[-1],acc)
+
+    DAT =  numpy.column_stack((rownames, rowvals))
+    numpy.savetxt(save_file_params, DAT, delimiter=",",fmt="%s")
+
+    return
+
+
+# In[57]:
+
+
+# test input combinations
+#-------------------------
+
+var_in = ["ref_i_ref_q","ref_i_dolp","ref_q_dolp","ref_i","ref_q","dolp"]
+var_in = ["ref_i_dolp"]
+
+for f in range(len(var_in)):
+    
+    # split train/test
+    X_train, X_test, y_train, y_test = split_data('D://ORACLES_NN//py_inputs//ER2_x_', 
+                                                  var_in[f], 
+                                                  var_out = 'D://ORACLES_NN//py_inputs//ER2_y_train_orig.csv')
+    # build model
+    rsp_nn_model = model(input_shape = X_train.shape[1:], 
+                         output_shape = y_test.shape[1])
+    
+    # run and validate model
+    run_model(rsp_nn_model, 
+              var_in[f], "ER2", X_train, X_test, y_train, y_test)
+    
+
+
+# ## 12. this step is to run hyperas hyperparameterization optimization on the best model inputs
